af4d0ebc79505dc66937a0aee3de7599cf15a436
Dale <dale.brooksby@gmail.com>
2020-05-17 22:50:28 +0100

    Initial commit

---
+++ Assets/EnemyBird.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBird : MonoBehaviour
+{
+
+    public float speed = 1.0f;
+    private Rigidbody2D rb;
+    private Vector2 screenBoundary;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(-speed, 0);
+        screenBoundary = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Is the bird to the left of the screen
+        if(transform.position.x < -screenBoundary.x * 2)
+        {
+            // Delete the bird off the screen
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/SpawnBirds.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBirds : MonoBehaviour
+{
+
+    public GameObject birdPrefab;
+    public float respawnTime = 5.0f;
+    private Vector2 screenBoundary;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenBoundary = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        StartCoroutine(birdFlock());
+    }
+    private void spawnBird()
+    {
+        GameObject a = Instantiate(birdPrefab) as GameObject;
+        a.transform.position = new Vector2(-screenBoundary.x * -2, Random.Range(-screenBoundary.y, screenBoundary.y + 5));
+    }
+
+    IEnumerator birdFlock()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(respawnTime);
+            spawnBird();
+        }
+    }
+}
